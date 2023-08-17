9368ff9ee1471f0bc35ada5999ec75d7772182ce
RK Sharma <78323004+rksiitd1@users.noreply.github.com>
2023-08-17 15:15:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// Example: A function to handle a form submission
+function handleFormSubmission(event) {
+    event.preventDefault();
+
+    // Logic to handle the form data
+}
+
+const form = document.querySelector("#contact-form");
+form.addEventListener("submit", handleFormSubmission);
